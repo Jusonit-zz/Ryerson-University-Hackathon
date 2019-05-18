ffce5db42c7b27a1df20fe8c4f0571481645b87d
Kubie <yanicakj@gmail.com>
2019-05-18 15:28:53 -0400

    changing db.php

--- db.php
+++ db.php
@@ -1,15 +1,28 @@
 <?php
 
-Class DB{
-    function get_connection(){
-        // STEP 01: Read configuration file
-        $config = json_decode(file_get_contents("config.json"), true);
+    Class DB{
+        function get_connection(){
+            // STEP 01: Read configuration file
+            $config = json_decode(file_get_contents("config.json"), true);
 
-        // STEP 02: Get Database Connection
-        $conn = new mysqli($config["servername"], $config["username"], $config["password"], $config["database"])
-            or die("<br>Could not connect to database");
+            // STEP 02: Get Database Connection
+            // $conn = new mysqli($config["servername"], $config["username"], $config["password"], $config["database"])
+            //     or die("<br>Could not connect to database");
+            $conn = new mysqli($config["servername"], $config["username"], $config["password"], $config["database"]);
 
-        // STEP 03: Return Connection
-        return $conn;
+            if ($conn) {
+                $result = "success";
+            } else {
+                $result = "failure";
+            }
+
+            // added this
+            $arr = array("results" => $result);
+            echo json_encode($arr);
+
+            // STEP 03: Return Connection
+            return $conn;
+        }
     }
-}
+
+?>
