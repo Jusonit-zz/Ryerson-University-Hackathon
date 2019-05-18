544ece683805f23f6908cb9cf286f2bfdd07bc13
rpasricha45 <rpasricha45@gmail.com>
2019-05-18 15:54:34 -0400

    popup - functionality
    - put in pop up for each of the evetns

--- script.js
+++ script.js
@@ -1,40 +1,46 @@
+
+
 document.addEventListener('click',function(e){
+            if (e.target && e.target.id== 'btn1') {
+              alert("creating and event");
+				document.getElementById('create').style.display = "block";
 
-    if (e.target && e.target.id== 'btn1') {
-
-    	document.getElementById('create').style.display = "block";
-
-    }
-    
-    else if (e.target && e.target.id== 'btn2') {
-
-        alert("connecting to database!");
-
-        // do something here when login button is clicked
-        let formData = new FormData();
-        formData.append('indicator', 'testconnection');
-
-        return fetch('35.196.158.3/accessDatabase.php', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            //body: JSON.stringify(JSON_DATA),
-            body: formData,
-            mode: 'no-cors'
-        })
-        .then((res) => res.json())
-        .then((response) => {
-            console.log(response);
-            //document.getElementById("div2").innerHTML = "<h1>Exams Need Grading</h1><br>" + response.ankit + "<br><button id='gradeexam' class=\"btn\" type=\"button\">View Exam</button><br<br><br><br>";
-        }); // end of fetch
-
-    }
-
-    //Function to Hide Popup
-    function div_hide(){
-        document.getElementById('create').style.display = "none";
-    }
+            }
+            else if (e.target && e.target.id== 'btn2') {
+                // do something here when create event button is clicked
+                alert("login or signup!");
+            }
+			//Function to Hide Popup
+			function div_hide(){
+			document.getElementById('create').style.display = "none";
+			}
+        }); // end of document listener
 
 
-    }); // end of document listener
+
+// add java sript of the event handlerers for the events the Modal
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
