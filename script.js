dbba076ecc13e47e127155c60106a3cb9c1f958e
rpasricha45 <rpasricha45@gmail.com>
2019-05-18 17:38:37 -0400

    popup -complete
    please fix - styling

--- script.js
+++ script.js
@@ -28,10 +28,21 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+// for the second buttons
+
+var btn2 = document.getElementById("myBtn2");
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
+
   modal.style.display = "block";
 }
+
+btn2.onclick = function(){
+
+  modal.style.display = "block";
+}
+
+
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
@@ -40,7 +51,9 @@ span.onclick = function() {
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
+
   if (event.target == modal) {
+
     modal.style.display = "none";
   }
 }
