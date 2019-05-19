9be05f4faf1ff8204d5ee81556eeb7e37eae8a0e
Junior <juniorsj@live.com>
2019-05-18 20:15:28 -0400

    Update login.php

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ function verify_credentials($user, $pass)
     $db = new DB();
     $conn = $db->get_connection();
     $result = $conn->query("SELECT * FROM profiles WHERE username = '$user' AND password = '$pass'");
-    $role = $result->num_rows > 0 ? $result->fetch_array()['role'] : 0;
+    $role = $result->num_rows > 0 ? $result->fetch_array()['ID'] : 0;
     $conn->close();
     return $role;
 }
